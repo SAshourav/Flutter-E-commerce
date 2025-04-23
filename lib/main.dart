3e6dcb8ee8c87c30e68352edd36e3912b794e705
SAshourav <sabbirahmed2013303@gmail.com>
2025-04-23 18:45:47 +0600

    splash style, and onboarding system

---
+++ lib/main.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:t_store/utils/theme/theme.dart';
+
+import 'app.dart';
+
+void main() {
+
+  // Todo : Add Widgets Binding
+  // Todo : Init Local Storage
+  // Todo : Await Native Splash
+  // Todo : Initialize Firebase
+  // Todo : Initialize Authentication
+
+  runApp(const App());
+}
+
+
+
