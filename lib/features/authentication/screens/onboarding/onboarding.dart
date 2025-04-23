4bbd7807db68d5b891c8d9d15e2a9e9ca7f5d1c9
SAshourav <sabbirahmed2013303@gmail.com>
2025-04-24 03:21:46 +0600

    test

--- lib/features/authentication/screens/onboarding/onboarding.dart
+++ lib/features/authentication/screens/onboarding/onboarding.dart
@@ -1,20 +1,26 @@
 import 'package:flutter/material.dart';
+import 'package:t_store/features/authentication/controllers/onboarding_controller.dart';
 import 'package:t_store/features/authentication/screens/onboarding/widgets/onboarding_dot_navigation.dart';
+import 'package:t_store/features/authentication/screens/onboarding/widgets/onboarding_next_button.dart';
 import 'package:t_store/features/authentication/screens/onboarding/widgets/onboarding_page.dart';
 import 'package:t_store/features/authentication/screens/onboarding/widgets/onboarding_skip.dart';
 import 'package:t_store/utils/constants/text_strings.dart';
 import '../../../../utils/constants/image_strings.dart';
+import 'package:get/get.dart';
 
 class OnBoardingScreen extends StatelessWidget {
   const OnBoardingScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
+    final controller = Get.put(OnBoardingController());
     return Scaffold(
       body: Stack(
         children: [
           /// Horizontal Scrollable Pages
           PageView(
+            controller: controller.pageController,
+            onPageChanged: controller.updatePageIndicator,
             children: const [
               OnBoardingPage(
                 images: TImages.onBoardingImage1,
@@ -40,10 +46,13 @@ class OnBoardingScreen extends StatelessWidget {
           const OnBoardingDotNavigation(),
 
           // Circular Button
+          const OnBoardingNextButton()
         ],
       ),
     );
   }
 }
+
+
 
 
--- lib/features/authentication/screens/onboarding/widgets/onboarding_dot_navigation.dart
+++ lib/features/authentication/screens/onboarding/widgets/onboarding_dot_navigation.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:smooth_page_indicator/smooth_page_indicator.dart';
+import 'package:t_store/features/authentication/controllers/onboarding_controller.dart';
 
 import '../../../../../utils/constants/colors.dart';
 import '../../../../../utils/constants/sizes.dart';
@@ -13,12 +14,14 @@ class OnBoardingDotNavigation extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final controller = OnBoardingController.instance;
     final dark = THelperFunctions.isDarkMode(context);
     return Positioned(
         bottom: TDeviceUtils.getBottomNavigationBarHeight() + 25,
         left: TSizes.defaultSpace,
         child: SmoothPageIndicator(
-          controller: PageController(),
+          controller: controller.pageController,
+          onDotClicked: controller.dotNavigationClick,
           count: 3,
           effect:  ExpandingDotsEffect(activeDotColor: dark ? TColors.light : TColors.dark, dotHeight: 6),
         )
