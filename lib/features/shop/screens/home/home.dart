f729abac18375e8c25f133ba0c652bc789bec7ac
SAshourav <sabbirahmed2013303@gmail.com>
2025-04-28 05:00:20 +0600

    search container and categories in home

--- lib/features/shop/screens/home/home.dart
+++ lib/features/shop/screens/home/home.dart
@@ -1,10 +1,19 @@
 import 'package:flutter/material.dart';
+import 'package:iconsax/iconsax.dart';
 import 'package:t_store/common/widgets/appbar/appbar.dart';
 import 'package:t_store/features/shop/screens/home/widgets/homeAppBar.dart';
+import 'package:t_store/features/shop/screens/home/widgets/home_categories.dart';
 import 'package:t_store/utils/constants/colors.dart';
+import 'package:t_store/utils/constants/sizes.dart';
+import 'package:t_store/utils/helpers/helper_functions.dart';
 import '../../../../common/widgets/custom_shapes/containers/primary_header_container.dart';
+import '../../../../common/widgets/custom_shapes/containers/search_container.dart';
+import '../../../../common/widgets/image_text_widgets/vertical_image_text.dart';
 import '../../../../common/widgets/products_cart/cart_menu_icon.dart';
+import '../../../../common/widgets/texts/section_heading.dart';
+import '../../../../utils/constants/image_strings.dart';
 import '../../../../utils/constants/text_strings.dart';
+import '../../../../utils/device/device_utility.dart';
 
 
 class HomeScreen extends StatelessWidget {
@@ -19,7 +28,26 @@ class HomeScreen extends StatelessWidget {
             PrimaryHeaderContainer(
               child: Column(
                 children: [
-                  THomeAppBar()
+                  /// AppBar
+                  THomeAppBar(),
+                  SizedBox(height: TSizes.spaceBtwSections,),
+                  /// Search Bar
+                  TSearchContainer(text: 'Search in store...',),
+                  SizedBox(height: TSizes.spaceBtwSections,),
+                  /// Categories
+                  Padding(
+                      padding: EdgeInsets.only(left: TSizes.defaultSpace),
+                    child: Column(
+                      children: [
+                        /// Heading
+                        TSectionHeading(title: 'Popular Categories', showActionButton: false, textColor: Colors.white,),
+                        SizedBox(height: TSizes.spaceBtwItems,),
+                        // Categories
+                        THomeCategories(),
+
+                      ],
+                    ),
+                  ),
                 ],
               )
             )
@@ -29,6 +57,13 @@ class HomeScreen extends StatelessWidget {
     );
   }
 }
+
+
+
+
+
+
+
 
 
 
