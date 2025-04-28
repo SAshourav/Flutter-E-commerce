65dbdda97972d3f673da5b2c82f3940c1b9a5d12
SAshourav <sabbirahmed2013303@gmail.com>
2025-04-28 06:17:40 +0600

    CarouselSlider

--- lib/features/shop/screens/home/home.dart
+++ lib/features/shop/screens/home/home.dart
@@ -1,14 +1,18 @@
+import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter/material.dart';
 import 'package:iconsax/iconsax.dart';
 import 'package:t_store/common/widgets/appbar/appbar.dart';
+import 'package:t_store/common/widgets/custom_shapes/containers/circular_container.dart';
 import 'package:t_store/features/shop/screens/home/widgets/homeAppBar.dart';
 import 'package:t_store/features/shop/screens/home/widgets/home_categories.dart';
+import 'package:t_store/features/shop/screens/home/widgets/promo_slider.dart';
 import 'package:t_store/utils/constants/colors.dart';
 import 'package:t_store/utils/constants/sizes.dart';
 import 'package:t_store/utils/helpers/helper_functions.dart';
 import '../../../../common/widgets/custom_shapes/containers/primary_header_container.dart';
 import '../../../../common/widgets/custom_shapes/containers/search_container.dart';
 import '../../../../common/widgets/image_text_widgets/vertical_image_text.dart';
+import '../../../../common/widgets/images/t_rounded_images.dart';
 import '../../../../common/widgets/products_cart/cart_menu_icon.dart';
 import '../../../../common/widgets/texts/section_heading.dart';
 import '../../../../utils/constants/image_strings.dart';
@@ -44,11 +48,17 @@ class HomeScreen extends StatelessWidget {
                         SizedBox(height: TSizes.spaceBtwItems,),
                         // Categories
                         THomeCategories(),
-
                       ],
                     ),
                   ),
                 ],
+              )
+            ),
+            Padding(
+              padding: EdgeInsets.all(TSizes.defaultSpace),
+              child: TpromoSlider(banners: [
+                TImages.promoBanner1, TImages.promoBanner2,TImages.promoBanner3
+              ],
               )
             )
           ],
@@ -57,6 +67,10 @@ class HomeScreen extends StatelessWidget {
     );
   }
 }
+
+
+
+
 
 
 
